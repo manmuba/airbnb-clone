6dba3aceecb899c5fa53961a7e7232190b4a6d6b
Mubarek Shikur <manmuba90@gmail.com>
2023-09-10 12:42:14 +0300

    nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 
 import { Nunito } from "next/font/google";
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Navbar />
         {children}
       </body>
     </html>
